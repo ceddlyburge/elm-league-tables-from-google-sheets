321ce7e093f70bb4b8f0d3c22c3804ece426e643
CeddlyBurge <ceddlyburge@gmail.com>
2018-07-04 07:55:11 +0100

    Move test file up one folder, to be directly under tests folder
    This is required because elm test looks in the test or tests folder, but does not look in subfolder. This commit also fixes up the json and assertion so that the test now passes

--- tests/DecodeGoogleSheetToLeagueListTest.elm
+++ tests/DecodeGoogleSheetToLeagueListTest.elm
@@ -11,12 +11,12 @@ import Updates.DecodeGoogleSheetToLeagueList exposing (decodeGoogleSheets)
 
 decodeSpreadsheetIdResponse : Test
 decodeSpreadsheetIdResponse  =
-    test "Sets Title property of League from Title property of google sheet / tab" <|
+    test "Sets League.Title from title property of google sheet / tab" <|
     \() ->
         let 
             decodedOuput = (decodeString decodeGoogleSheets spreadsheetIdResponse)
         in
-            Expect.equal decodedOuput (Ok ([ (League "Regional Div 1"), (League "blah") ]))
+            Expect.equal decodedOuput (Ok ([ (League "Regional Div 1"), (League "Regional Div 2") ]))
 
 
 -- This is a cut down response from the test spreadsheet, at https://sheets.googleapis.com/v4/spreadsheets/1Ai9H6Pfe1LPsOcksN6EF03-z-gO1CkNp8P1Im37N3TE?key=<thekey>
@@ -26,24 +26,23 @@ spreadsheetIdResponse =
     "spreadsheetId": "blah",
     "properties": {
         "title": "Canoe Polo League Test Scores ",
-        "locale": "etc",
-        }
+        "locale": "etc"
     },
     "sheets": [
-        {
+      {
         "properties": {
             "sheetId": 1,
             "title": "Regional Div 1",
-            "etc": 0,
+            "etc": 0
         }
-        },
-        {
+      },
+      {
         "properties": {
             "sheetId": 1,
             "title": "Regional Div 2",
-            "etc": 0,
+            "etc": 0
         }
-        }
+      }
     ],
     "spreadsheetUrl": "blah"
   }"""
