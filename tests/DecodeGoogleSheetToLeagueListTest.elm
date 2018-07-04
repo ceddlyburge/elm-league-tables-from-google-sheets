eb343c6a350b555bc4963bd12bb224edc596bdc8
CeddlyBurge <ceddlyburge@gmail.com>
2018-07-04 18:47:06 +0100

    Refactor test to avoid temporary variable

--- tests/DecodeGoogleSheetToLeagueListTest.elm
+++ tests/DecodeGoogleSheetToLeagueListTest.elm
@@ -13,11 +13,8 @@ decodeSpreadsheetIdResponse : Test
 decodeSpreadsheetIdResponse  =
     test "Sets League.Title from title property of google sheet / tab" <|
     \() ->
-        let 
-            decodedOuput = (decodeString decodeGoogleSheets spreadsheetIdResponse)
-        in
-            Expect.equal decodedOuput (Ok ([ (League "Regional Div 1"), (League "Regional Div 2") ]))
-
+        decodeString decodeGoogleSheets spreadsheetIdResponse
+        |> Expect.equal (Ok ([ (League "Regional Div 1"), (League "Regional Div 2") ]))
 
 -- This is a cut down response from the test spreadsheet, at https://sheets.googleapis.com/v4/spreadsheets/1Ai9H6Pfe1LPsOcksN6EF03-z-gO1CkNp8P1Im37N3TE?key=<thekey>
 spreadsheetIdResponse: String
