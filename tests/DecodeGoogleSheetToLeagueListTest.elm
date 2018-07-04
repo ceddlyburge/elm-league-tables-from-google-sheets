b2b35c76d9692b5c6e9a2a687d7a4251d1b00a47
CeddlyBurge <ceddlyburge@gmail.com>
2018-07-04 18:53:18 +0100

    Refactor test to avoid temporary variable

--- tests/DecodeGoogleSheetToLeagueListTest.elm
+++ tests/DecodeGoogleSheetToLeagueListTest.elm
@@ -16,6 +16,24 @@ decodeSpreadsheetIdResponse  =
         decodeString decodeGoogleSheets spreadsheetIdResponse
         |> Expect.equal (Ok ([ (League "Regional Div 1"), (League "Regional Div 2") ]))
 
+-- decodeSpreadsheetIdResponse2 : Test
+-- decodeSpreadsheetIdResponse2  =
+--     fuzz List string "2 Sets League.Title from title property of google sheet / tab" <|
+--     \() ->
+--         decodeString decodeGoogleSheets spreadsheetIdResponse
+--         |> Expect.equal (Ok ([ (League "Regional Div 1"), (League "Regional Div 2") ]))
+
+
+sheetWithTitle: String -> String
+sheetWithTitle title =
+    """{
+    "properties": {
+        "sheetId": 1,
+        "title": """ ++ title ++ """,
+        "etc": 0
+    }
+    }"""
+
 -- This is a cut down response from the test spreadsheet, at https://sheets.googleapis.com/v4/spreadsheets/1Ai9H6Pfe1LPsOcksN6EF03-z-gO1CkNp8P1Im37N3TE?key=<thekey>
 spreadsheetIdResponse: String
 spreadsheetIdResponse =
@@ -26,20 +44,8 @@ spreadsheetIdResponse =
         "locale": "etc"
     },
     "sheets": [
-      {
-        "properties": {
-            "sheetId": 1,
-            "title": "Regional Div 1",
-            "etc": 0
-        }
-      },
-      {
-        "properties": {
-            "sheetId": 1,
-            "title": "Regional Div 2",
-            "etc": 0
-        }
-      }
+      """ ++ sheetWithTitle "Regional Div 1" ++ """,
+      """ ++ sheetWithTitle "Regional Div 2" ++ """,
     ],
     "spreadsheetUrl": "blah"
   }"""
