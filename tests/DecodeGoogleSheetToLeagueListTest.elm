74848b6d5465e7c11d79bef82fd48f1db0f43eb6
CeddlyBurge <ceddlyburge@gmail.com>
2018-07-05 08:05:26 +0100

    Fuzz test google sheet to League decoder

--- tests/DecodeGoogleSheetToLeagueListTest.elm
+++ tests/DecodeGoogleSheetToLeagueListTest.elm
@@ -1,42 +1,26 @@
 module DecodeGoogleSheetToLeagueListTest exposing (..)
 
 import Test exposing (..)
+import Fuzz exposing (Fuzzer, list, string)
 import Expect
 import Json.Decode exposing (decodeString)
+import Json.Encode exposing (encode, string)
 
 import Models.League exposing (League)
 import Updates.DecodeGoogleSheetToLeagueList exposing (decodeGoogleSheets)
 
--- use the api response we already have, might want to move it somewhere so it can be more easily shared between end to end tests and unit tests
-
 decodeSpreadsheetIdResponse : Test
 decodeSpreadsheetIdResponse  =
-    test "Sets League.Title from title property of google sheet / tab" <|
-    \() ->
-        decodeString decodeGoogleSheets spreadsheetIdResponse
-        |> Expect.equal (Ok ([ (League "Regional Div 1"), (League "Regional Div 2") ]))
+    fuzz (list Fuzz.string) "Sets League.Title from title property of google sheet / tab" <|
+    \(leagueTitles) ->
+        spreadsheetIdResponseWithSheetNames leagueTitles
+        |> decodeString decodeGoogleSheets
+        |> Expect.equal (Ok (List.map League leagueTitles))
 
--- decodeSpreadsheetIdResponse2 : Test
--- decodeSpreadsheetIdResponse2  =
---     fuzz List string "2 Sets League.Title from title property of google sheet / tab" <|
---     \() ->
---         decodeString decodeGoogleSheets spreadsheetIdResponse
---         |> Expect.equal (Ok ([ (League "Regional Div 1"), (League "Regional Div 2") ]))
-
-
-sheetWithTitle: String -> String
-sheetWithTitle title =
-    """{
-    "properties": {
-        "sheetId": 1,
-        "title":\"""" ++ title ++ """\",
-        "etc": 0
-    }
-    }"""
 
 -- This is a cut down response from the test spreadsheet, at https://sheets.googleapis.com/v4/spreadsheets/1Ai9H6Pfe1LPsOcksN6EF03-z-gO1CkNp8P1Im37N3TE?key=<thekey>
-spreadsheetIdResponse: String
-spreadsheetIdResponse =
+spreadsheetIdResponseWithSheetNames: List String -> String
+spreadsheetIdResponseWithSheetNames leagueTitles =
   """{
     "spreadsheetId": "blah",
     "properties": {
@@ -44,8 +28,22 @@ spreadsheetIdResponse =
         "locale": "etc"
     },
     "sheets": [
-      """ ++ (sheetWithTitle "Regional Div 1") ++ """,
-      """ ++ (sheetWithTitle "Regional Div 2") ++ """
+      """ ++ sheetsWithTitles leagueTitles ++ """
     ],
     "spreadsheetUrl": "blah"
   }"""
+
+sheetsWithTitles: List String -> String
+sheetsWithTitles leagueTitles =
+    String.join "," (List.map sheetWithTitle leagueTitles)
+  
+sheetWithTitle: String -> String
+sheetWithTitle title =
+    """{
+    "properties": {
+        "sheetId": 1,
+        "title":""" ++ encode 0 (Json.Encode.string title) ++ """,
+        "etc": 0
+    }
+    }"""
+
