0e87911e73802dc409249a785518fcf11fd3b31e
CeddlyBurge <ceddlyburge@gmail.com>
2018-07-11 17:29:24 +0100

    Pull up pattern matching in update function

--- src/LeagueList/Updates.elm
+++ src/LeagueList/Updates.elm
@@ -17,26 +17,23 @@ update msg model =
         Messages.Msg.SheetRequest ->
             sheetRequest model 
 
-        Messages.Msg.SheetResponse result ->
-            sheetRespond result model
+        Messages.Msg.SheetResponse (Err httpError) ->
+            logErrorAndNoOp httpError model
+
+        Messages.Msg.SheetResponse (Ok leagues) ->
+            ( { model | leagues = leagues}, Cmd.none )
 
 sheetRequest : Model -> ( Model, Cmd Msg )
 sheetRequest model =
     ( model, Http.send Messages.Msg.SheetResponse (request model.config) )
 
-request : Config -> Http.Request (List League)
+request : Config -> Http.Request ( List League )
 request config =
     Http.get ("https://sheets.googleapis.com/v4/spreadsheets/" ++ config.googleSheet ++ "?key=" ++ config.googleApiKey) decodeGoogleSheets
 
-sheetRespond : (Result Http.Error (List League)) -> Model -> ( Model, Cmd Msg )
-sheetRespond result model =
-    case result of
-        Err httpError ->
-            let
-                _ =
-                    Debug.log "sheetResponseError" httpError
-            in
-                ( model, Cmd.none )
-
-        Ok leagues ->
-            ( { model | leagues = leagues}, Cmd.none )
+logErrorAndNoOp : Http.Error -> Model -> ( Model, Cmd Msg )
+logErrorAndNoOp httpError model =
+    let
+        _ = Debug.log "sheetResponseError" httpError
+    in
+        ( model, Cmd.none )
