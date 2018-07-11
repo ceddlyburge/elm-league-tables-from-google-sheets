5ed70c9f40eb4bcf1800cefca5459203d3307721
CeddlyBurge <ceddlyburge@gmail.com>
2018-07-11 17:37:15 +0100

    Add test for api success response

--- tests/LeagueListUpdate.elm
+++ tests/LeagueListUpdate.elm
@@ -3,18 +3,27 @@ module LeagueListUpdate exposing (..)
 import Http exposing (..)
 
 import Test exposing (..)
+import Fuzz exposing (list, string)
 import Expect
 
 import LeagueList.Updates exposing (update)
 import Messages.Msg exposing ( .. )
 import Models.Config exposing (Config)
 import Models.Model exposing (Model)
+import Models.League exposing (League)
 
 apiError : Test
 apiError =
     test "Returns model and cmd.none on error" <|
         \() -> update ( Messages.Msg.SheetResponse ( Err NetworkError ) ) model
             |> Expect.equal (model, Cmd.none)
+
+apiSuccess : Test
+apiSuccess =
+    fuzz ( list string ) "Returns model and Leagues on success" <|
+        \( leagues ) -> update ( Messages.Msg.SheetResponse ( Ok ( List.map League leagues ) ) ) model
+            |> \( model, cmd ) -> model.leagues
+            |> Expect.equal ( List.map League leagues )
 
 model : Model
 model = 
