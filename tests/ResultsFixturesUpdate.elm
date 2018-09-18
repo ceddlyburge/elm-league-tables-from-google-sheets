5a47000de435798b2ed2d71ad0c1817f9fd9f679
CeddlyBurge <ceddlyburge@gmail.com>
2018-09-18 08:16:06 +0100

    Add a failing unit test for ResultsFixtures update

---
+++ tests/ResultsFixturesUpdate.elm
@@ -0,0 +1,28 @@
+module ResultsFixturesUpdate exposing (..)
+
+import Test exposing (..)
+import Expect
+import RemoteData exposing (WebData)
+import Update exposing (update)
+import Msg exposing (..)
+import Models.Model exposing (Model, vanillaModel)
+import Models.LeagueTable exposing (LeagueTable)
+import Models.Game exposing (Game, LeagueGames)
+import Models.Team exposing (Team)
+
+apiSuccess : Test
+apiSuccess =
+    test "Updates model with LeagueGames on success" <|
+        \() ->
+            update (IndividualSheetResponseForResultsFixtures "" leagueGames) vanillaModel
+            |> \(model, msg) -> model.leagueGames
+            |> Expect.equal leagueGames
+
+leagueGames: LeagueGames
+leagueGames = 
+    RemoteData.Success ( LeagueGames "Div 1" [ game ] )
+
+game: Game
+game = 
+    Game "Castle" (Just 3) "Meridian" (Just 1) "2018-06-04" "1, 6, 4" "2" "Green 3, Yellow 5" "Red 14" "good game"
+
