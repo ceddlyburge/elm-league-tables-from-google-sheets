65972391fd2662b26cd505692e587d461c05690b
CeddlyBurge <ceddlyburge@gmail.com>
2018-09-20 17:59:34 +0100

    Fix url and leagueGames in ResultsFixtures update

--- src/ResultsFixtures/Update.elm
+++ src/ResultsFixtures/Update.elm
@@ -13,16 +13,18 @@ import UpdateSharedFunctions exposing (fetchIndividualSheet)
 
 individualSheetRequestForResultsFixtures : String -> Model -> ( Model, Cmd Msg )
 individualSheetRequestForResultsFixtures leagueTitle model  =
-    ( { model | leagueTable = RemoteData.Loading }, fetchLeagueGames leagueTitle model.config )
+    ( { model | leagueGames = RemoteData.Loading }, fetchLeagueGames leagueTitle model.config )
 
 individualSheetResponseForResultsFixtures : Model -> WebData LeagueGames -> String -> ( Model, Cmd Msg )
 individualSheetResponseForResultsFixtures  model response leagueTitle =
+    -- probably define a new shared function that takes the model and returns the newUrl with the route in the model
+    -- this will reduce duplication and make it impossible to mismatch the change in urls
     ( 
         { model | 
             route = Route.ResultsFixturesRoute leagueTitle
             , leagueGames = response 
         }
-        , newUrl <| toUrl <| Route.LeagueTableRoute leagueTitle
+        , newUrl <| toUrl <| Route.ResultsFixturesRoute leagueTitle
     )
 
 fetchLeagueGames : String -> Config -> Cmd Msg
