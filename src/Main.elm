f186caac5e04799189f473f50c4b47be7a3af8f1
CeddlyBurge <ceddlyburge@gmail.com>
2018-11-05 17:49:31 +0000

    Log location on app load
    Calling the api directly on app load works, so the progressive problem must be in the location I think. This commit logs all the location properties, which I will inspect for differences when using the app in progressive mode in chrome and when using normally

--- src/Main.elm
+++ src/Main.elm
@@ -11,8 +11,6 @@ import Update exposing (update)
 import View exposing (view)
 import Navigation exposing (Location)
 import Subscriptions
-import Pages.LeagueList.Update exposing (allSheetSummaryRequest)
-
 
 -- It is not possible to import this module in to an elm-spec test, as it tells me there is a circular dependency.
 -- There isn't one that I can see, so it must be created by the test runner or something like that.
@@ -23,12 +21,23 @@ import Pages.LeagueList.Update exposing (allSheetSummaryRequest)
 init : Config -> Location -> ( Model, Cmd Msg )
 init config location =
     let
+        _ = Debug.log "href" location.href
+        _ = Debug.log "host" location.host
+        _ = Debug.log "hostname" location.hostname
+        _ = Debug.log "protocol" location.protocol
+        _ = Debug.log "origin" location.origin
+        _ = Debug.log "port_" location.port_
+        _ = Debug.log "pathnamee" location.pathname
+        _ = Debug.log "search" location.search
+        _ = Debug.log "hash" location.hash
+        _ = Debug.log "username" location.username
+        _ = Debug.log "password" location.password
         (model, cmd) = 
             update 
             (Msg.OnLocationChange location)
             { vanillaModel | config = config }
     in
-        allSheetSummaryRequest model --Cmd.batch [cmd, Task.perform Msg.SetScreenSize Window.size] )
+        (model, Cmd.batch [cmd, Task.perform Msg.SetScreenSize Window.size] )
      
 
 
