e38d71f7a77330445b1da5944235290f8e2c9fd7
CeddlyBurge <ceddlyburge@gmail.com>
2018-11-12 07:53:45 +0000

    Remove duplication in end to end test json responses

--- EndToEndTests/SpreadsheetValuesResponse.elm
+++ EndToEndTests/SpreadsheetValuesResponse.elm
@@ -1,5 +1,6 @@
 module SpreadsheetValuesResponse exposing (spreadsheetValuesResponse)
 
+import ApiResponseFragments exposing (..)
 
 -- This is the based on the response from the test spreadsheet, at https://sheets.googleapis.com/v4/spreadsheets/1Ai9H6Pfe1LPsOcksN6EF03-z-gO1CkNp8P1Im37N3TE/values/Regional%20Div%201?key=<thekey>
 spreadsheetValuesResponse: String
@@ -8,19 +9,9 @@ spreadsheetValuesResponse =
     "range": "'Regional Div 1'!A1:Z1000",
     "majorDimension": "ROWS",
     "values": [
-      [
-        "Home Team",
-        "Home Score",
-        "Away Score",
-        "Away Team",
-        "Date Played",
-        "Home Scorers",
-        "Away Scorers",
-        "Home Cards",
-        "Away Cards",
-        "Notes"
-      ],
-      [
+  """
+  ++ completeHeaderRow ++
+  """, [
         "Castle",
         "3",
         "0",
--- EndToEndTests/SpreadsheetValuesWithOneUnplayedGameResponse.elm
+++ EndToEndTests/SpreadsheetValuesWithOneUnplayedGameResponse.elm
@@ -1,5 +1,6 @@
 module SpreadsheetValuesWithOneUnplayedGameResponse exposing (spreadsheetValuesWithOneUnplayedGameResponse)
 
+import ApiResponseFragments exposing (..)
 
 -- This is the based on the response from the test spreadsheet, at https://sheets.googleapis.com/v4/spreadsheets/1Ai9H6Pfe1LPsOcksN6EF03-z-gO1CkNp8P1Im37N3TE/values/Regional%20Div%201?key=<thekey>
 spreadsheetValuesWithOneUnplayedGameResponse: String
@@ -8,19 +9,9 @@ spreadsheetValuesWithOneUnplayedGameResponse =
     "range": "'Regional Div 1'!A1:Z1000",
     "majorDimension": "ROWS",
     "values": [
-      [
-        "Home Team",
-        "Home Score",
-        "Away Score",
-        "Away Team",
-        "Date Played",
-        "Home Scorers",
-        "Away Scorers",
-        "Home Cards",
-        "Away Cards",
-        "Notes"
-      ],
-      [
+  """
+  ++ completeHeaderRow ++
+  """, [
         "Castle",
         "",
         "",
