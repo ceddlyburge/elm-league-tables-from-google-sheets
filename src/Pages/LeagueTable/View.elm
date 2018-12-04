daceba855011d2cca3520bdc64b44738c87f3722
CeddlyBurge <ceddlyburge@gmail.com>
2018-12-04 19:30:46 +0000

    Add column definitions for all screen widths

--- src/Pages/LeagueTable/View.elm
+++ src/Pages/LeagueTable/View.elm
@@ -17,88 +17,104 @@ import Pages.HeaderBarItem exposing (..)
 
 -- maybe split out column to its own file, might well be useful elsewhere
 type alias Column =
-    { title: String
-        , value: Team -> String
-        , width: Int -- make this a float, I think it would make things easier, although might still be the odd annoyance.
-        , priority : Int
+    { value: Team -> String
+    , cssClass: String
+    , element : Styles -> List (Element.Attribute Variations Msg) -> Element.Element Styles Variations Msg -> Element.Element Styles Variations Msg
+    , title: String
+    , width: Int -- make this a float, I think it would make things easier, although might still be the odd annoyance.
+    , priority : Int
     }
 
--- the biggest small width thing is "Played" in the full titles, or "P" in the condensed titles
--- LeagueTableTeamRow defines small font size, which is 12, 15, 18 or 24 points
--- width of played in this font size for font (plus a bit) is roughly  56px, 65px, 85px, 115 px  
+multiline: Styles -> List (Element.Attribute Variations Msg) -> Element.Element Styles Variations Msg -> Element.Element Styles Variations Msg
+multiline styles attributes element =
+    paragraph styles attributes [element]
+
+-- Pixel widths, with one character spare, measured using https://codepen.io/jasesmith/pen/eBeoNz
 allColumns : Device -> List Column
 allColumns device =
-    -- "Pts" is 25px in 12pt font (font size is from LeagueStyleElements)
     if device.width <= 600 then
-        []
-        -- [     Column ""     position       (px 30) 
-        --     , Column "Team" name           fill 
-        --     , Column "P"    gamesPlayed    (px 30) 
-        --     , Column "W"    won            (px 30) 
-        --     , Column "D"    drawn          (px 30) 
-        --     , Column "L"    lost           (px 30) 
-        --     , Column "GD"   goalDifference (px 30) 
-        --     , Column "Pts"  points         (px 30) 
-        -- ]
+        -- 12px font (font size is from LeagueStyleElements)
+        [     position       el        ""     21  8
+            , team           multiline "Team" 100 9
+            , played         el        "P"    21  6
+            , won            el        "W"    21  1
+            , drawn          el        "D"    21  1
+            , lost           el        "L"    21  1
+            , goalsFor       el        "GF"   21  0
+            , goalsAgainst   el        "GA"   21  0 
+            , goalDifference el        "GD"   21  5
+            , points         el        "Pts"  26  7
+        ]
     else if device.width <= 1200 then
-        columns600To1200px device.width
+        -- 15px font (from LeagueStyleElements)
+        [     position       el        ""                26  8
+            , team           multiline "Team"            150 9
+            , played         el        "Played"          57  6
+            , won            el        "Won"             41  1
+            , drawn          el        "Drawn"           56  1
+            , lost           el        "Lost"            41  1
+            , goalsFor       multiline "Goals For"       49  0
+            , goalsAgainst   multiline "Goals Against"   65  0 
+            , goalDifference multiline "Goal Difference" 83  5
+            , points         el        "Points"          53  7
+        ]
     else if device.width <= 1800 then
-        []
-        -- [     Column ""                position       (px 80) 
-        --     , Column "Team"            name           (px 200) 
-        --     , Column "Played"          gamesPlayed    (px 80) 
-        --     , Column "Won"             won            (px 80) 
-        --     , Column "Drawn"           drawn          (px 80) 
-        --     , Column "Lost"            lost           (px 80) 
-        --     , Column "Goals For"       goalsFor       (px 80) 
-        --     , Column "Goals Against"   goalsAgainst   (px 80) 
-        --     , Column "Goal Difference" goalDifference (px 120) 
-        --     , Column "Points"          points         (px 80) 
-        -- ]
+        -- 18px font (from LeagueStyleElements)
+        [     position       el        ""                31  8
+            , team           multiline "Team"            200 9
+            , played         el        "Played"          69  6
+            , won            el        "Won"             49  1
+            , drawn          el        "Drawn"           68  1
+            , lost           el        "Lost"            49  1
+            , goalsFor       multiline "Goals For"       59  0
+            , goalsAgainst   multiline "Goals Against"   78  0 
+            , goalDifference multiline "Goal Difference" 99  5
+            , points         el        "Points"          63  7
+        ]
     else 
-        []
-        -- [     Column ""                position       (px 120) 
-        --     , Column "Team"            name           (px 300) 
-        --     , Column "Played"          gamesPlayed    (px 120) 
-        --     , Column "Won"             won            (px 120) 
-        --     , Column "Drawn"           drawn          (px 120) 
-        --     , Column "Lost"            lost           (px 120) 
-        --     , Column "Goals For"       goalsFor       (px 120) 
-        --     , Column "Goals Against"   goalsAgainst   (px 120) 
-        --     , Column "Goal Difference" goalDifference (px 180) 
-        --     , Column "Points"          points         (px 120) 
-        -- ]
+        -- 24px font (from LeagueStyleElements)
+        [     position       el        ""                41  8
+            , team           multiline "Team"            300 9
+            , played         el        "Played"          92  6
+            , won            el        "Won"             66  1
+            , drawn          el        "Drawn"           90  1
+            , lost           el        "Lost"            65  1
+            , goalsFor       multiline "Goals For"       79  0
+            , goalsAgainst   multiline "Goals Against"   104 0 
+            , goalDifference multiline "Goal Difference" 132 5
+            , points         el        "Points"          85  7
+        ]
 
--- 15pt font (from LeagueStyleElements)
--- Pixel widths, with one character spare
--- (Postion)  35
--- Team       65 (team name should be bigger though so probably 115 at least)
--- Played     77 
--- Won        55
--- Drawn      75
--- Lost       54
--- Goals      66
--- Against    87
--- Difference 110
--- Points     70
--- total      694 (but this is with team name too small, so probably 744 at least)
-columns600To1200px: Int -> List Column
-columns600To1200px width =
-    [     Column ""                position       35  8
-        , Column "Team"            name           165 9
-        , Column "Played"          gamesPlayed    77  6
-        , Column "Won"             won            55  1
-        , Column "Drawn"           drawn          75  1
-        , Column "Lost"            lost           54  1
-        , Column "Goals For"       goalsFor       66  0
-        , Column "Goals Against"   goalsAgainst   87  0
-        , Column "Goal Difference" goalDifference 110 5
-        , Column "Points"          points         70  7
-    ]
+position = 
+    Column Models.Team.position "position"
 
--- this will remove items from the top of the list, which is not what I want
--- can't really sort, as then the ui order goes wrong
--- could filter probably. yep use filter. and probably map then max to get the priority
+team = 
+    Column Models.Team.name "name"
+
+played = 
+    Column Models.Team.gamesPlayed "gamesPlayed"
+
+won = 
+    Column Models.Team.won "won"
+
+drawn =
+    Column Models.Team.drawn "drawn"
+
+lost =
+    Column Models.Team.lost "lost"
+
+goalsFor =
+    Column Models.Team.goalsFor "goalsFor"
+
+goalsAgainst =
+    Column Models.Team.goalsAgainst "goalsAgainst"
+
+goalDifference =
+    Column Models.Team.goalDifference "goalDifference"
+
+points =
+    Column Models.Team.points "points"
+
 columnsAvailableForWidth: Int -> Int -> Int -> List Column -> List Column
 columnsAvailableForWidth width padding spacing columns =
     let
@@ -140,7 +156,7 @@ page leagueTitle response device =
         ( maybeResponse response (leagueTableElement device) )
 
 
-leagueTableElement : Device -> LeagueTable -> Element Styles variation Msg
+leagueTableElement : Device -> LeagueTable -> Element Styles Variations Msg
 leagueTableElement device leagueTable =
     let
         gaps = gapsForDevice device
@@ -156,160 +172,30 @@ leagueTableElement device leagueTable =
             (List.map (teamRow columns device gaps) leagueTable.teams)
         )
 
-headerRow : List Column -> Device -> Gaps -> Element Styles variation Msg
+headerRow : List Column -> Device -> Gaps -> Element Styles Variations Msg
 headerRow tableColumns device gaps = 
     row 
         LeagueTableHeaderRow 
         [ padding gaps.medium, spacing gaps.small, center ] 
         (List.map headerCell tableColumns)
 
-headerCell : Column -> Element Styles variation Msg
+headerCell : Column -> Element Styles Variations Msg
 headerCell column =
-    el 
+    column.element 
         None 
-        [ width (px (toFloat column.width)), class "need to do this: cssClass" ] 
+        [ width (px (toFloat column.width)), class column.cssClass ] 
         (text column.title)
 
-teamRow : List Column -> Device -> Gaps -> Team -> Element Styles variation Msg
+teamRow : List Column -> Device -> Gaps -> Team -> Element Styles Variations Msg
 teamRow tableColumns device gaps team = 
     row 
         LeagueTableTeamRow 
         [ padding gaps.medium, spacing gaps.small, center, class "team" ] 
         (List.map (teamCell team) tableColumns)
 
-teamCell : Team -> Column -> Element Styles variation Msg
+teamCell : Team -> Column -> Element Styles Variations Msg
 teamCell team column=
-    el 
+    column.element 
         None 
-        [ width (px (toFloat column.width)), class "need to do this: cssClass" ] 
+        [ width (px (toFloat column.width)), class column.cssClass ] 
         (text <| column.value team)
-
--- type alias LeagueTableHeaderText = 
---     { position: String
---         , team: String
---         , played: String
---         , won: String
---         , drawn: String
---         , lost: String
---         , goalsFor: String
---         , goalsAgainst: String
---         , goalDifference: String
---         , points: String
---     }
-
--- headerRow2 : Device -> Gaps -> Element Styles variation Msg
--- headerRow2 device gaps = 
---     let
---         headers = headerText device
---     in
---         row LeagueTableHeaderRow [ padding gaps.medium, spacing gaps.small, center ] 
---         [
---             small device "" headers.position
---             , big device "" headers.team
---             , small device "" headers.played
---             , small device "" headers.won
---             , small device "" headers.drawn
---             , small device "" headers.lost
---             , smallWithWrap device "" headers.goalsFor
---             , smallWithWrap device "" headers.goalsAgainst
---             , mediumWithWrap device "" headers.goalDifference
---             , small device "" headers.points
---         ]
-
-
--- teamRow2 : Device -> Gaps -> Team -> Element Styles variation Msg
--- teamRow2 device gaps team = 
---     row LeagueTableTeamRow [ padding gaps.medium, spacing gaps.small, center, class "team" ] 
---     [ 
---         small device "" "0"  -- team.position
---         , bigWithWrap device "name" team.name
---         , small device "gamesPlayed" (toString team.gamesPlayed)
---         , small device "" "0" -- headers.won
---         , small device "" "0" -- headers.drawn
---         , small device "" "0" -- headers.lost
---         , small device "goalsFor" (toString team.goalsFor)
---         , small device "goalsAgainst" (toString team.goalsAgainst)
---         , medium device "goalDifference" (toString team.goalDifference)
---         , small device "points" (toString team.points)
---     ]
-
--- small: Device -> String -> String -> Element Styles variation Msg
--- small  device cssClass cellContents = 
---     el None [ smallWidth device, class cssClass ] (text cellContents)
-
--- smallWithWrap: Device -> String -> String -> Element Styles variation Msg
--- smallWithWrap  device cssClass cellContents = 
---     paragraph None [ smallWidth device, class cssClass ] [text cellContents]
-
--- medium: Device -> String -> String -> Element Styles variation Msg
--- medium device cssClass cellContents = 
---     el None [ mediumWidth device, class cssClass ] (text cellContents)
-
--- mediumWithWrap: Device -> String -> String -> Element Styles variation Msg
--- mediumWithWrap  device cssClass cellContents = 
---     paragraph None [ mediumWidth device, class cssClass ] [text cellContents]
-
--- big: Device -> String -> String -> Element Styles variation Msg
--- big device cssClass cellContents = 
---     el None [ bigWidth device, class cssClass ] (text cellContents)
-
--- bigWithWrap: Device -> String -> String -> Element Styles variation Msg
--- bigWithWrap  device cssClass cellContents = 
---     paragraph None [ bigWidth device, class cssClass ] [text cellContents]
-
--- headerText: Device -> LeagueTableHeaderText
--- headerText device = 
---     if device.width < 600 then
---         LeagueTableHeaderText
---             ""
---             "Team"
---             "P"
---             "W"
---             "D"
---             "L"
---             ""
---             ""
---             "GD"
---             "Pts"
---     else
---         LeagueTableHeaderText
---             ""
---             "Team"
---             "Played"
---             "Won"
---             "Drawn"
---             "Lost"
---             "Goals For"
---             "Goals Against"
---             "Goal Difference"
---             "Points"
-
--- the biggest small width thing is "Played" in the full titles, or "P" in the condensed titles
--- LeagueTableTeamRow defines small font size, which is 12, 15, 18 or 24 points
--- width of played in this font size for font (plus a bit) is roughly  56px, 65px, 85px, 115 px  
--- smallWidth: Device -> Element.Attribute variation msg
--- smallWidth device = 
---     if device.phone then
---         width (px 20)
---     else
---         width (px 80)
-
--- the biggest medium width thing is "Difference" in the full titles, assuming that "Goals Difference" wraps, or "Pts" in the condensed titles
--- LeagueTableTeamRow defines small font size, which is 12, 15, 18 or 24 points
--- width of played in this font size for font (plus a bit) is roughly  70px, 105px, 125px, 170px  
--- mediumWidth: Device -> Element.Attribute variation msg
--- mediumWidth device = 
---     if device.phone then
---         width (px 20)
---     else
---         width (px 120)
-
--- big width is for the team name, which could be anything, but can wrap. Let's say "Meridian X" should definitely be on one line
--- LeagueTableTeamRow defines small font size, which is 12, 15, 18 or 24 points
--- width of played in this font size for font (plus a bit) is roughly  95px, 120px, 145px, 195px  
--- bigWidth: Device -> Element.Attribute variation msg
--- bigWidth device = 
---     if device.phone then
---         width fill
---     else
---         width (px 200)
