2b492ebebb9c120ff9178bfce6c7974a9d6eacd4
CeddlyBurge <ceddlyburge@gmail.com>
2018-12-04 17:58:00 +0000

    Add accessor functions
    This makes the view code a lot neater.
    
    I think it is best having the functions with the data
    type they are used for, as it makes them more
    likely to be reused.

--- src/Models/Team.elm
+++ src/Models/Team.elm
@@ -1,4 +1,4 @@
-module Models.Team exposing (Team)
+module Models.Team exposing (..)
 
 
 type alias Team =
@@ -9,3 +9,44 @@ type alias Team =
     , goalsAgainst : Int
     , goalDifference : Int
     }
+
+position: Team -> String
+position team = 
+    "0"
+
+name: Team -> String
+name team = 
+    team.name
+
+gamesPlayed: Team -> String
+gamesPlayed team = 
+    toString team.gamesPlayed
+
+points: Team -> String
+points team = 
+    toString team.points
+
+goalsFor: Team -> String
+goalsFor team = 
+    toString team.goalsFor
+
+goalsAgainst: Team -> String
+goalsAgainst team = 
+    toString team.goalsAgainst
+
+goalDifference: Team -> String
+goalDifference team = 
+    toString team.goalDifference
+
+won: Team -> String
+won team = 
+    "0"
+
+drawn: Team -> String
+drawn team = 
+    "0"
+
+lost: Team -> String
+lost team = 
+    "0"
+
