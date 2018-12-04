80482c8acf439e3a5033b55b733d076815fb6310
CeddlyBurge <ceddlyburge@gmail.com>
2018-12-04 20:14:03 +0000

    Split responsive column code in to its own file

--- src/Pages/LeagueTable/View.elm
+++ src/Pages/LeagueTable/View.elm
@@ -14,20 +14,65 @@ import Pages.MaybeResponse exposing (..)
 import Pages.Page exposing (..)
 import Pages.HeaderBar exposing (..) 
 import Pages.HeaderBarItem exposing (..)
+import Pages.ResponsiveColumn exposing (..)
 
--- maybe split out column to its own file, might well be useful elsewhere
-type alias Column =
-    { value: Team -> String
-    , cssClass: String
-    , element : Styles -> List (Element.Attribute Variations Msg) -> Element.Element Styles Variations Msg -> Element.Element Styles Variations Msg
-    , title: String
-    , width: Int -- make this a float, I think it would make things easier, although might still be the odd annoyance.
-    , priority : Int
-    }
 
-multiline: Styles -> List (Element.Attribute Variations Msg) -> Element.Element Styles Variations Msg -> Element.Element Styles Variations Msg
-multiline styles attributes element =
-    paragraph styles attributes [element]
+page : String -> WebData LeagueTable -> Device -> Page
+page leagueTitle response device =
+    Page
+        ( SingleHeader <| 
+            HeaderBar 
+                [ BackHeaderButton AllSheetSummaryRequest
+                , ResultsFixturesHeaderButton <| IndividualSheetRequestForResultsFixtures leagueTitle ] 
+                (Maybe.withDefault "" (decodeUri leagueTitle))
+                [ RefreshHeaderButton <| IndividualSheetRequest leagueTitle ] )
+        ( maybeResponse response (leagueTableElement device) )
+
+
+leagueTableElement : Device -> LeagueTable -> Element Styles Variations Msg
+leagueTableElement device leagueTable =
+    let
+        gaps = gapsForDevice device
+        columns = respondedColumns 
+            device.width 
+            gaps.medium 
+            gaps.small  
+            (allColumns device)
+    in
+        column None [ class "teams" ]
+        (
+            [ headerRow columns device gaps ]
+            ++ 
+            (List.map (teamRow columns device gaps) leagueTable.teams)
+        )
+
+headerRow : List Column -> Device -> Gaps -> Element Styles Variations Msg
+headerRow tableColumns device gaps = 
+    row 
+        LeagueTableHeaderRow 
+        [ padding gaps.medium, spacing gaps.small, center ] 
+        (List.map headerCell tableColumns)
+
+headerCell : Column -> Element Styles Variations Msg
+headerCell column =
+    column.element 
+        None 
+        [ width (px column.width), class column.cssClass ] 
+        (text column.title)
+
+teamRow : List Column -> Device -> Gaps -> Team -> Element Styles Variations Msg
+teamRow tableColumns device gaps team = 
+    row 
+        LeagueTableTeamRow 
+        [ padding gaps.medium, spacing gaps.small, center, class "team" ] 
+        (List.map (teamCell team) tableColumns)
+
+teamCell : Team -> Column -> Element Styles Variations Msg
+teamCell team column=
+    column.element 
+        None 
+        [ width (px column.width), class column.cssClass ] 
+        (text <| column.value team)
 
 -- Pixel widths, with one character spare, measured using https://codepen.io/jasesmith/pen/eBeoNz
 allColumns : Device -> List Column
@@ -85,6 +130,8 @@ allColumns device =
             , points         el        "Points"          85  7
         ]
 
+-- the type signature for these is extremely onerous, and I haven't found a 
+-- way of making it better, so I'm just leaving them out for now.
 position = 
     Column Models.Team.position "position"
 
@@ -114,88 +161,3 @@ goalDifference =
 
 points =
     Column Models.Team.points "points"
-
-columnsAvailableForWidth: Int -> Int -> Int -> List Column -> List Column
-columnsAvailableForWidth width padding spacing columns =
-    let
-        lowestPriority = Maybe.withDefault 0 (List.minimum (List.map (\column -> column.priority) columns))
-    in
-        if  columnsWidth padding spacing columns <= width then
-            columns
-        else
-            columnsAvailableForWidth width padding spacing (List.filter (\column -> not (column.priority == lowestPriority)) columns)
-
-columnsWidth : Int -> Int -> List Column -> Int
-columnsWidth padding spacing columns =
-    (List.foldr (\column total -> total + column.width) 0 columns)
-    + padding + padding + ((List.length columns) * spacing)
-
--- split out a padColumn function
-padColumns: Int -> Int -> Int -> List Column -> List Column
-padColumns width padding spacing columns  = 
-    let
-        numberOfColumns = List.length columns
-        allColumnsWidth = columnsWidth padding spacing columns
-        availableWidth = width - padding - padding - (numberOfColumns * spacing)
-        desiredWidth = round ((toFloat availableWidth) * 0.8)
-    in
-        if allColumnsWidth < desiredWidth then
-            List.map (\column -> { column | width = column.width + round( (toFloat (desiredWidth - allColumnsWidth)) / toFloat numberOfColumns )}) columns 
-        else
-            columns
-
-page : String -> WebData LeagueTable -> Device -> Page
-page leagueTitle response device =
-    Page
-        ( SingleHeader <| 
-            HeaderBar 
-                [ BackHeaderButton AllSheetSummaryRequest
-                , ResultsFixturesHeaderButton <| IndividualSheetRequestForResultsFixtures leagueTitle ] 
-                (Maybe.withDefault "" (decodeUri leagueTitle))
-                [ RefreshHeaderButton <| IndividualSheetRequest leagueTitle ] )
-        ( maybeResponse response (leagueTableElement device) )
-
-
-leagueTableElement : Device -> LeagueTable -> Element Styles Variations Msg
-leagueTableElement device leagueTable =
-    let
-        gaps = gapsForDevice device
-        columns = 
-            allColumns device 
-            |> columnsAvailableForWidth device.width (round gaps.medium) (round gaps.small) 
-            |> padColumns device.width (round gaps.medium) (round gaps.small)
-    in
-        column None [ class "teams" ]
-        (
-            [ headerRow columns device gaps ]
-            ++ 
-            (List.map (teamRow columns device gaps) leagueTable.teams)
-        )
-
-headerRow : List Column -> Device -> Gaps -> Element Styles Variations Msg
-headerRow tableColumns device gaps = 
-    row 
-        LeagueTableHeaderRow 
-        [ padding gaps.medium, spacing gaps.small, center ] 
-        (List.map headerCell tableColumns)
-
-headerCell : Column -> Element Styles Variations Msg
-headerCell column =
-    column.element 
-        None 
-        [ width (px (toFloat column.width)), class column.cssClass ] 
-        (text column.title)
-
-teamRow : List Column -> Device -> Gaps -> Team -> Element Styles Variations Msg
-teamRow tableColumns device gaps team = 
-    row 
-        LeagueTableTeamRow 
-        [ padding gaps.medium, spacing gaps.small, center, class "team" ] 
-        (List.map (teamCell team) tableColumns)
-
-teamCell : Team -> Column -> Element Styles Variations Msg
-teamCell team column=
-    column.element 
-        None 
-        [ width (px (toFloat column.width)), class column.cssClass ] 
-        (text <| column.value team)
---
+++ src/Pages/ResponsiveColumn.elm
@@ -0,0 +1,70 @@
+module Pages.ResponsiveColumn exposing (Column, multiline, respondedColumns)
+
+import Element exposing (..)
+
+import Models.Team exposing (Team)
+import LeagueStyleElements exposing (..)
+import Msg exposing (..)
+
+type alias Column =
+    { value: Team -> String
+    , cssClass: String
+    , element : Styles -> List (Element.Attribute Variations Msg) -> Element.Element Styles Variations Msg -> Element.Element Styles Variations Msg
+    , title: String
+    , width: Float
+    , priority : Int
+    }
+
+respondedColumns: Int -> Float -> Float -> List Column -> List Column
+respondedColumns width padding spacing columns =
+    columns
+    |> columnsAvailableForWidth width padding spacing 
+    |> padColumns width padding spacing
+
+columnsAvailableForWidth: Int -> Float -> Float -> List Column -> List Column
+columnsAvailableForWidth width padding spacing columns =
+    let
+        lowestPriority = Maybe.withDefault 0 (List.minimum <| List.map priority columns)
+    in
+        if  columnsWidth padding spacing columns <= toFloat width then
+            columns
+        else
+            columnsAvailableForWidth 
+                width 
+                padding 
+                spacing 
+                (List.filter (isNotLowestPriority lowestPriority) columns)
+
+columnsWidth : Float -> Float -> List Column -> Float
+columnsWidth padding spacing columns =
+    (List.foldr addWidth 0 columns)
+    + padding + padding + (toFloat (List.length columns) * spacing)
+
+padColumns: Int -> Float -> Float -> List Column -> List Column
+padColumns width padding spacing columns  = 
+    let
+        numberOfColumns = toFloat (List.length columns)
+        allColumnsWidth = columnsWidth padding spacing columns
+        availableWidth = (toFloat) width - padding - padding - (numberOfColumns * spacing)
+        desiredWidth = availableWidth * 0.8
+    in
+        if allColumnsWidth < desiredWidth then
+            List.map (\column -> { column | width = column.width + (desiredWidth - allColumnsWidth) / numberOfColumns}) columns 
+        else
+            columns
+
+multiline: Styles -> List (Element.Attribute Variations Msg) -> Element.Element Styles Variations Msg -> Element.Element Styles Variations Msg
+multiline styles attributes element =
+    paragraph styles attributes [element]
+
+priority: Column -> Int
+priority column =
+    column.priority
+
+isNotLowestPriority: Int -> Column -> Bool
+isNotLowestPriority lowestPriority column =
+    not (column.priority == lowestPriority)
+
+addWidth: Column -> Float -> Float
+addWidth column width  =
+    column.width + width 
