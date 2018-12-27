e27ee8410f61dc8b8795d5f1b6a2349b3c555525
CeddlyBurge <ceddlyburge@gmail.com>
2018-12-27 19:05:52 +0000

    End to end test order of games within a day

--- EndToEndTests/ResultsAndFixtures.elm
+++ EndToEndTests/ResultsAndFixtures.elm
@@ -1,7 +1,7 @@
 import Spec exposing (..)
 
 import SpreadsheetIdResponses exposing (..)
-import SpreadsheetValuesOneResultTwoFixturesResponse exposing (..)
+import SpreadsheetValuesOneResultThreeFixturesResponse exposing (..)
 
 import Models.Model exposing ( Model, vanillaModel )
 import Models.Config exposing ( Config )
@@ -21,7 +21,7 @@ specs =
             },
             { method = "GET"
             , url = "https://sheets.googleapis.com/v4/spreadsheets/spreadSheetId/values/Regional Div 1?key=googleApiKey"
-            , response = { status = 200, body = spreadsheetValuesOneResultTwoFixturesResponse }
+            , response = { status = 200, body = spreadsheetValuesOneResultThreeFixturesResponse }
             }
           ]
         
@@ -29,6 +29,7 @@ specs =
           [ steps.click ".data-test-refresh"
           , steps.click ".data-test-league" -- only one league in the results
           , steps.click ".data-test-resultsAndFixtures" -- shows results and fixtures page
+
           -- these assert the order of the days
           , assert.classPresent
             { selector = ".data-test-dates .data-test-day:nth-Child(1)"
@@ -42,24 +43,42 @@ specs =
             { selector = ".data-test-dates .data-test-day:nth-Child(3)"
             , class = "data-test-date-unscheduled"
             }
+
           -- these assert the fixtures / results for each day
           -- create a function for these selectors to remove duplication? makes it harder to read, but less likely to have typos. hmmm.
           , assert.containsText
             { selector = ".data-test-dates .data-test-date-2018-06-04 .data-test-game:nth-Child(1) .data-test-homeTeamName"
-            , text = "Castle"
+            , text = "Blackwater"
             }
           , assert.containsText
             { selector = ".data-test-dates .data-test-date-2018-06-04 .data-test-game:nth-Child(1) .data-test-homeTeamGoals"
-            , text = "3"
+            , text = "2"
             }
           , assert.containsText
             { selector = ".data-test-dates .data-test-date-2018-06-04 .data-test-game:nth-Child(1) .data-test-awayTeamGoals"
-            , text = "0"
+            , text = "1"
             }          
           , assert.containsText
             { selector = ".data-test-dates .data-test-date-2018-06-04 .data-test-game:nth-Child(1) .data-test-awayTeamName"
+            , text = "Clapham"
+            }
+          , assert.containsText
+            { selector = ".data-test-dates .data-test-date-2018-06-04 .data-test-game:nth-Child(2) .data-test-homeTeamName"
+            , text = "Castle"
+            }
+          , assert.containsText
+            { selector = ".data-test-dates .data-test-date-2018-06-04 .data-test-game:nth-Child(2) .data-test-homeTeamGoals"
+            , text = "3"
+            }
+          , assert.containsText
+            { selector = ".data-test-dates .data-test-date-2018-06-04 .data-test-game:nth-Child(2) .data-test-awayTeamGoals"
+            , text = "0"
+            }          
+          , assert.containsText
+            { selector = ".data-test-dates .data-test-date-2018-06-04 .data-test-game:nth-Child(2) .data-test-awayTeamName"
             , text = "Meridian"
             }
+
           , assert.containsText
             { selector = ".data-test-dates .data-test-date-2018-06-03 .data-test-game:nth-Child(1) .data-test-homeTeamName"
             , text = "Battersea"
--- EndToEndTests/SpreadsheetValuesOneResultThreeFixturesResponse.elm
+++ EndToEndTests/SpreadsheetValuesOneResultThreeFixturesResponse.elm
@@ -1,9 +1,9 @@
-module SpreadsheetValuesOneResultTwoFixturesResponse exposing (spreadsheetValuesOneResultTwoFixturesResponse)
+module SpreadsheetValuesOneResultThreeFixturesResponse exposing (spreadsheetValuesOneResultThreeFixturesResponse)
 
 
 -- This is the based on the response from the test spreadsheet, at https://sheets.googleapis.com/v4/spreadsheets/1Ai9H6Pfe1LPsOcksN6EF03-z-gO1CkNp8P1Im37N3TE/values/Regional%20Div%201?key=<thekey>
-spreadsheetValuesOneResultTwoFixturesResponse: String
-spreadsheetValuesOneResultTwoFixturesResponse =
+spreadsheetValuesOneResultThreeFixturesResponse: String
+spreadsheetValuesOneResultThreeFixturesResponse =
   """{
     "range": "'Regional Div 1'!A1:Z1000",
     "majorDimension": "ROWS",
@@ -23,10 +23,17 @@ spreadsheetValuesOneResultTwoFixturesResponse =
       ],
       [
         "Castle",
+        "2",
+        "1",
+        "Meridian",
+        "2018-06-04 10:40"
+      ],
+      [
+        "Blackwater",
         "3",
         "0",
-        "Meridian",
-        "2018-06-04"
+        "Clapham",
+        "2018-06-04 10:20"
       ],
       [
         "Battersea",
