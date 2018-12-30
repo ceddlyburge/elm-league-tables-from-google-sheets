da0359bb831ac82742e71efdf9afb88238b386e9
CeddlyBurge <ceddlyburge@gmail.com>
2018-12-30 13:36:26 +0000

    Make sure the build fails if any of the end to end tests fail
    This wasn't happening before. ooops.

--- EndToEndTests/end-to-end-tests.sh
+++ EndToEndTests/end-to-end-tests.sh
@@ -1,5 +1,10 @@
+err=0
+trap 'err=1' ERR
+
 elm-spec ResultsAndFixtures.elm
 elm-spec LeagueWithDataEntryIssues.elm
 elm-spec LeagueList.elm
 elm-spec LeagueWithOneUnplayedGame.elm
 elm-spec LeagueWithOneGame.elm
+
+test $err = 0 # Return non-zero if any command failed
