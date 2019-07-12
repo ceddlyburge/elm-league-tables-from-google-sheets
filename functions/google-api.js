b6be6666ba0fb440b6ebf6e53d142f5098398874
CeddlyBurge <ceddlyburge@gmail.com>
2019-07-12 17:40:26 +0100

    Use netlify function for all sheets request, as well as per sheet request
    This doesn't work well locally, as the proxying in create-elm-app isn't
    working. I've tried and failed to debug this.

--- functions/google-api.js
+++ functions/google-api.js
@@ -1,48 +1,81 @@
-// from https://github.com/depadiernos/token-hider-inator
 const axios = require("axios")
 
 export function handler(event, context, callback) {
-  const { ELM_APP_GOOGLE_SHEET, ELM_APP_GOOGLE_API_KEY } = process.env 
-  const URL = `https://sheets.googleapis.com/v4/spreadsheets/${ELM_APP_GOOGLE_SHEET}/values/${event.queryStringParameters.leagueTitle}?key=${ELM_APP_GOOGLE_API_KEY}` 
+  const ELM_APP_GOOGLE_SHEET = process.env.ELM_APP_GOOGLE_SHEET
+  const ELM_APP_GOOGLE_API_KEY = process.env.ELM_APP_GOOGLE_API_KEY
+  //const URL = `https://sheets.googleapis.com/v4/spreadsheets/${ELM_APP_GOOGLE_SHEET}/values/${event.queryStringParameters.leagueTitle}?key=${ELM_APP_GOOGLE_API_KEY}` 
+  const requiredReponseHeaders = { 
+    "content-type": "application/json; charset=UTF-8",
+    "access-control-allow-origin": "*",
+    "access-control-expose-headers": "content-encoding,date,server,content-length"
+  }
+  const requiredRequestHeaders = { 
+    "Accept": "application/json",
+    "accept": "application/json",
+    "Host": "sheets.googleapis.com",
+    "host": "sheets.googleapis.com"
+  }
 
-  console.log(event)
-  console.log(context)
-
-  const pass = (body) => {
-    callback( null, {
-      statusCode: 200,
-      headers: { 
-        "content-type": "application/json; charset=UTF-8",
-        "access-control-allow-origin": "*",
-        "access-control-expose-headers": "content-encoding,date,server,content-length"
-      },
-      body: JSON.stringify(body)
-    })
+  const url = () => {
+    if (event.queryStringParameters.leagueTitle) {
+      return `https://sheets.googleapis.com/v4/spreadsheets/${ELM_APP_GOOGLE_SHEET}/values/${event.queryStringParameters.leagueTitle}?key=${ELM_APP_GOOGLE_API_KEY}`
+    } else {
+      return `https://sheets.googleapis.com/v4/spreadsheets/${ELM_APP_GOOGLE_SHEET}?key=${ELM_APP_GOOGLE_API_KEY}`
+    }
   }
 
   const get = () => {
-    console.log("Perform the API call to ", URL)
-    const newHeaders = {
-        "Accept": "application/json",
-        "Host": "sheets.googleapis.com",
-        "accept": "application/json",
-        "host": "sheets.googleapis.com",
-    }
-    const headers = {...event.headers, ...newHeaders}
-    console.log(headers)
-    axios.get(URL, {
-      headers: headers
-    })
-    .then((response) =>
-      {
-        pass(response.data)
+    console.log("Perform the API call to ", url())
+    axios.get(
+      url(),
+      { headers: {...event.headers, ...requiredRequestHeaders} 
       }
-    )
-    .catch(pass)
+    ).then(success)
+    .catch(failure)
   }
-  if(event.httpMethod == 'GET'){
+
+  const success = (response) => {
+    console.log(response)
+    callback( null, {
+      statusCode: response.status,
+      // The required headers are already returned by the google api
+      headers: {...requiredReponseHeaders, ...response.headers},
+      body: JSON.stringify(response.data)
+    })
+  }
+
+  const failure = (error) => {
+    console.log(error)
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      callback( null, {
+        statusCode: error.response.status,
+        headers: {...requiredReponseHeaders, ...error.response.headers},
+        body: JSON.stringify(error.response.data)
+      })
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      callback( null, {
+        statusCode: 408, // Request Timeout seems most appropriate
+        headers: requiredReponseHeaders,
+        body: JSON.stringify("No response received from Google Api")
+      })
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      callback( null, {
+        statusCode: 500,
+        headers: requiredReponseHeaders,
+        body: JSON.stringify(error.message)
+      })
+    }
+  }
+
+  if(event.httpMethod == 'GET') {
     get()
-  };
+  }
 };
 
 // exports.handler = (event, context, callback) => {
