db7917e159273d2781a79a68f9d0307ebda54d4e
CeddlyBurge <ceddlyburge@gmail.com>
2019-07-15 10:30:38 +0100

    Remove dead wood

--- src/Ports.elm
+++
@@ -1,7 +0,0 @@
-port module Ports exposing (..)
-
-import Models.LeagueSummary exposing (LeagueSummary)
---import Models.LeagueGames exposing (LeagueGames)
-
-port storeLeagues : List LeagueSummary -> Cmd msg
---port storeLeagueGames : LeagueGames -> Cmd msg
