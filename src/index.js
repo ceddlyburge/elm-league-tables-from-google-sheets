45788872b9bf5d16cb538f02f4b479c5fe3a7eb4
CeddlyBurge <ceddlyburge@gmail.com>
2019-07-21 21:15:40 +0100

    Add default for netlify functions server
    This default is an empty string, which will end up looking for a relative path, which will work on the deployment an netlify, and could work in development if a proxy was set up and working. I haven't got the proxy working though, so I set it to a different server

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Main } from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
 
 var app = Main.embed(document.getElementById('root'), {
-    netlifyFunctionsServer: process.env.ELM_APP_NETLIFY_FUNCTIONS_SERVER
+    netlifyFunctionsServer: process.env.ELM_APP_NETLIFY_FUNCTIONS_SERVER || ""
 });
 
 registerServiceWorker();
