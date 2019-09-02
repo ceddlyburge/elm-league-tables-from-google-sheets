be1cee40b3ada15a5464feb078d59a3ee490b72c
ceddlyburge <ceddlyburge@gmail.com>
2019-09-02 21:17:31 +0100

    Fix import of vanillaGame

--- tests/ResultsFixturesHelpers.elm
+++ tests/ResultsFixturesHelpers.elm
@@ -5,7 +5,7 @@ import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, intRange, list)
 import Date.Extra exposing (..)
 
-import Models.Game exposing (Game)
+import Models.Game exposing (Game, vanillaGame)
 import Models.ResultsFixtures exposing (ResultsFixtures)
 import Models.LeagueGamesForDay exposing (LeagueGamesForDay)
 
