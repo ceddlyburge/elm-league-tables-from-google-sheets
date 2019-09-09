1699d957bce92d9c97224dc76e2c8bee78b1d77f
CeddlyBurge <ceddlyburge@gmail.com>
2019-09-09 19:21:23 +0100

    Add tests for UpdateHelpers
    These are basically the same as the existing update tests, and mean that
    I can mostly delete them.

---
+++ tests/UpdateHelpersTest.elm
@@ -0,0 +1,92 @@
+module UpdateHelpersTest exposing (..)
+
+import Dict exposing (Dict)
+import Test exposing (..)
+import Expect
+import Expect exposing (Expectation)
+import RemoteData exposing (WebData)
+import Msg exposing (..)
+import Models.Model exposing (Model, vanillaModel)
+import Models.Game exposing (vanillaGame)
+import Models.LeagueGames exposing (LeagueGames)
+import Models.LeagueTable exposing (..)
+import Models.ResultsFixtures exposing (..)
+import Models.Route as Route exposing (Route)
+import Pages.UpdateHelpers exposing (..)
+
+-- I am missing a test for individualSheetResponse
+
+-- This only tests one Route, should maybe extend it to test others
+-- It also only tests with a vanilla model, as opposed to a partially
+-- filled model. The code doesn't currently partially fill the model,
+-- and cachesAPiResultschecks it a bit, so I think its ok.
+callsApi : Test
+callsApi =
+    test "Calls the APi if the results arent already available in the model" <|
+        \() ->
+            showRouteRequiringIndividualSheetApi
+                leagueTitle
+                (Route.ResultsFixtures leagueTitle)
+                vanillaModel
+            |> getModel
+            |> Expect.equal 
+                { vanillaModel | 
+                    leagueTables = Dict.singleton leagueTitle RemoteData.Loading
+                    , resultsFixtures = Dict.singleton leagueTitle RemoteData.Loading
+                    , players = Dict.singleton leagueTitle RemoteData.Loading
+                    , route = Route.ResultsFixtures leagueTitle }
+
+cachesAPiResult : Test
+cachesAPiResult =
+    test "Only calls the api if the results isn't already available in the model" <|
+        \() ->
+            let 
+                model = 
+                    { vanillaModel | 
+                        leagueTables = Dict.singleton leagueTitle (RemoteData.Success vanillaLeagueTable)
+                        , resultsFixtures = Dict.singleton leagueTitle (RemoteData.Success vanillaResultsFixtures)
+                        , players = Dict.singleton leagueTitle (RemoteData.Success [])
+                    }
+            in 
+                showRouteRequiringIndividualSheetApi
+                    leagueTitle
+                    (Route.LeagueTable leagueTitle)
+                    model
+                |> getModel
+                |> Expect.equal { model | route = Route.LeagueTable leagueTitle }
+
+refreshesApi : Test
+refreshesApi =
+    test "Calls the APi if asked to, even if the data already exists" <|
+        \() ->
+            let 
+                model = 
+                    { vanillaModel | 
+                        leagueTables = Dict.singleton leagueTitle (RemoteData.Success vanillaLeagueTable)
+                        , resultsFixtures = Dict.singleton leagueTitle (RemoteData.Success vanillaResultsFixtures)
+                        , players = Dict.singleton leagueTitle (RemoteData.Success [])
+                    }
+            in 
+                refreshRouteRequiringIndividualSheetApi
+                    leagueTitle
+                    (Route.TopScorers leagueTitle)
+                    vanillaModel
+                |> getModel
+                |> Expect.equal 
+                    { model | 
+                        leagueTables = Dict.singleton leagueTitle RemoteData.Loading
+                        , resultsFixtures = Dict.singleton leagueTitle RemoteData.Loading
+                        , players = Dict.singleton leagueTitle RemoteData.Loading
+                        , route = Route.TopScorers leagueTitle }
+
+leagueTitle : String                
+leagueTitle =
+    "Regional Div 1"
+
+leagueGames: LeagueGames
+leagueGames = 
+     LeagueGames leagueTitle [ vanillaGame ]
+
+getModel : (Model, Cmd Msg) -> Model
+getModel (model, cmd) = 
+    model
