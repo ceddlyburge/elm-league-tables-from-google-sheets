2d058423ecbd37c065f2de19a5d61a7604e51750
Eduard Popescu <eduard.popescu@ness.com>
2020-01-14 10:59:02 +0200

    Added test and fixed a failing test.

--- tests/CalculateFixturesResultsFromLeagueGamesTest.elm
+++ tests/CalculateFixturesResultsFromLeagueGamesTest.elm
@@ -9,7 +9,7 @@ import Expect exposing (Expectation)
 import List.Extra exposing (gatherWith)
 import Models.LeagueGames exposing (LeagueGames)
 import Models.ResultsFixtures exposing (ResultsFixtures)
-import Calculations.ResultsFixturesFromLeagueGames exposing (calculateResultsFixtures)
+import Calculations.ResultsFixturesFromLeagueGames exposing (calculateResultsFixtures, playerOccurrences)
 import ResultsFixturesHelpers exposing (..)
 
 
@@ -19,28 +19,36 @@ groupsGamesByDay =
         \(dateTimes) ->
             let
                 games = List.map scheduledGame dateTimes
-                groupedDates = 
+                groupedDates =
                     List.map (Time.Extra.floor Day utc) dateTimes
                     |> List.sortWith comparePosix
                     |> List.reverse
                     |> gatherWith (==)
-            in    
+            in
                 calculateResultsFixtures (LeagueGames "Any League Title" games)
-                |> expectNumberOfGamesForDates 
-                        (List.map 
-                            (\(firstDate, remainingDates) 
-                                -> GamesForDay (Just firstDate) (1 + (List.length remainingDates))) 
+                |> expectNumberOfGamesForDates
+                        (List.map
+                            (\(firstDate, remainingDates)
+                                -> GamesForDay (Just firstDate) (1 + List.length remainingDates))
                             groupedDates)
 
 type alias GamesForDay =
     { date: Maybe Posix
-    , numberOfGames: Int 
+    , numberOfGames: Int
     }
 
 expectNumberOfGamesForDates: List GamesForDay -> ResultsFixtures -> Expectation
 expectNumberOfGamesForDates expectedNumberOfDaysFordateTimes resultsFixtures =
     let
         actualNumberOfDaysFordateTimes = List.map (\leagueGamesForDay -> GamesForDay leagueGamesForDay.date (List.length leagueGamesForDay.games )) resultsFixtures.days
-    in    
+    in
         Expect.equalLists expectedNumberOfDaysFordateTimes actualNumberOfDaysFordateTimes
-    
+
+playerOccurrencesTest : Test
+playerOccurrencesTest =
+    test  "Groups player occurrences" <|
+        \() ->
+            playerOccurrences
+                [ "john", "mike", "ed", "cedd", "mike", "cedd", "john", "john" ]
+            |> Expect.equal
+                [("cedd", 2), ("ed", 1), ("john", 3), ("mike", 2)]
