2aa1ce17c1acfc88cbf8c17b65c4a1e0733474fa
Cedd Burge <ceddlyburge@gmail.com>
2023-04-26 16:55:35 +0100

    Upgrade some dependencies

---
+++ cypress/e2e/league-table.js
@@ -0,0 +1,103 @@
+describe("The League Table Page", function () {
+  it("shows league table for one completely entered, played game", function () {
+    cy.intercept(
+      "GET",
+      "/.netlify/functions/google-api?leagueTitle=Regional%20Div%201",
+      { fixture: "one-played-game.json" }
+    );
+
+    // I could visit the home here and then click on the link to get to the league table page
+    // this would be a better end to end test, but risks repeating the navigating testing a
+    // lot, and means I would have to set up another route
+    // I think I'll test the navigation separately
+    cy.visit("/league/Regional%20Div%201");
+
+    cy.get(".data-test-teams .data-test-team:nth-Child(2)").within(() => {
+      cy.get(".data-test-position").contains("1");
+      cy.get(".data-test-name").contains("Castle");
+      cy.get(".data-test-gamesPlayed").contains("1");
+      cy.get(".data-test-won").contains("1");
+      cy.get(".data-test-drawn").contains("0");
+      cy.get(".data-test-lost").contains("0");
+      cy.get(".data-test-points").contains("3");
+      cy.get(".data-test-goalsFor").contains("3");
+      cy.get(".data-test-goalsAgainst").contains("0");
+      cy.get(".data-test-goalDifference").contains("3");
+    });
+
+    cy.get(".data-test-teams .data-test-team:nth-Child(3)").within(() => {
+      cy.get(".data-test-position").contains("2");
+      cy.get(".data-test-name").contains("Meridian");
+      cy.get(".data-test-gamesPlayed").contains("1");
+      cy.get(".data-test-won").contains("0");
+      cy.get(".data-test-drawn").contains("0");
+      cy.get(".data-test-lost").contains("1");
+      cy.get(".data-test-points").contains("0");
+      cy.get(".data-test-goalsFor").contains("0");
+      cy.get(".data-test-goalsAgainst").contains("3");
+      cy.get(".data-test-goalDifference").contains("-3");
+    });
+  });
+
+  it("shows league table for one unplayed game", function () {
+    cy.intercept(
+      "GET",
+      "/.netlify/functions/google-api?leagueTitle=Regional%20Div%201",
+      { fixture: "one-unplayed-game.json" }
+    );
+
+    // I could visit the home here and then click on the link to get to the league table page
+    // this would be a better end to end test, but risks repeating the navigating testing a
+    // lot, and means I would have to set up another route
+    // I think I'll test the navigation separately
+    cy.visit("/league/Regional%20Div%201");
+
+    cy.get(".data-test-teams .data-test-team:nth-Child(2)").within(() => {
+      cy.get(".data-test-position").contains("1");
+      cy.get(".data-test-name").contains("Castle");
+      cy.get(".data-test-gamesPlayed").contains("0");
+      cy.get(".data-test-won").contains("0");
+      cy.get(".data-test-drawn").contains("0");
+      cy.get(".data-test-lost").contains("0");
+      cy.get(".data-test-points").contains("0");
+      cy.get(".data-test-goalsFor").contains("0");
+      cy.get(".data-test-goalsAgainst").contains("0");
+      cy.get(".data-test-goalDifference").contains("0");
+    });
+
+    cy.get(".data-test-teams .data-test-team:nth-Child(3)").within(() => {
+      cy.get(".data-test-position").contains("2");
+      cy.get(".data-test-name").contains("Meridian");
+      cy.get(".data-test-gamesPlayed").contains("0");
+      cy.get(".data-test-won").contains("0");
+      cy.get(".data-test-drawn").contains("0");
+      cy.get(".data-test-lost").contains("0");
+      cy.get(".data-test-points").contains("0");
+      cy.get(".data-test-goalsFor").contains("0");
+      cy.get(".data-test-goalsAgainst").contains("0");
+      cy.get(".data-test-goalDifference").contains("0");
+    });
+  });
+
+  it("ignores invalid games and whitespace", function () {
+    cy.intercept(
+      "GET",
+      "/.netlify/functions/google-api?leagueTitle=Regional%20Div%201",
+      { fixture: "one-unplayed-game.json" }
+    );
+
+    // I could visit the home here and then click on the link to get to the league table page
+    // this would be a better end to end test, but risks repeating the navigating testing a
+    // lot, and means I would have to set up another route
+    // I think I'll test the navigation separately
+    cy.visit("/league/Regional%20Div%201");
+
+    cy.get(
+      ".data-test-teams .data-test-team:nth-Child(2) .data-test-name"
+    ).contains("Castle");
+
+    cy.get(
+      ".data-test-teams .data-test-team:nth-Child(3) .data-test-name"
+    ).contains("Meridian");
+  });
+});
